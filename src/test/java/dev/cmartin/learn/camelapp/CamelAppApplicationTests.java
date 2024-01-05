86a228d5646b32bb2f36e8d8a114653d88613fbd
Carlos Martin <cmarting@gmail.com>
2024-01-05 16:57:50 +0100

    initial commit

---
+++ src/test/java/dev/cmartin/learn/camelapp/CamelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cmartin.learn.camelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
