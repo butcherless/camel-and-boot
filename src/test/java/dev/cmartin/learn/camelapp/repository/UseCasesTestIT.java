1c516e63b728ade7279b0663103c09f90a34da1a
Carlos Martin <cmarting@gmail.com>
2026-06-18 21:26:21 +0200

    Fix import path for DataJpaTest annotation

--- src/test/java/dev/cmartin/learn/camelapp/repository/UseCasesTestIT.java
+++ src/test/java/dev/cmartin/learn/camelapp/repository/UseCasesTestIT.java
@@ -6,7 +6,7 @@ import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.data.jpa.DataJpaTest;
 
 import java.util.List;
 
